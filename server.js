efa54d8fb3b9dbfbd69f1895a23d6f60094c8bab
sjco <satyanarayana.j@cognizant.com>
2016-03-30 14:06:03 +0530

    added server and client modules

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+
+var routes = require("./server/web/rest/routes");
+
+app.use(express.static(__dirname+'/client'));
+app.use("/api",routes);
+app.get("*",function(req,res){
+	res.sendFile(__dirname+"/client/index.html");
+}).listen(3000);
+
+console.log("Server started at 3000!!!");
